4ff16f6d0aa5f0e93be135398334b518407b1450
matkrin <matkrin@protonmail.com>
2025-08-02 09:22:57 +0200

    feat(code action): Put on single line

--- server/code_action.go
+++ server/code_action.go
@@ -1,8 +1,11 @@
 package server
 
 import (
+	"bytes"
+
 	"github.com/matkrin/bashd/lsp"
 	"mvdan.cc/sh/v3/fileutil"
+	"mvdan.cc/sh/v3/syntax"
 )
 
 var SHEBANG = "#!/usr/bin/env bash\n\n"
@@ -15,8 +18,10 @@ func handleCodeAction(request *lsp.CodeActionRequest, state *State) *lsp.CodeAct
 	actions := []lsp.CodeAction{}
 	if !hasShebang {
 		action := shebangCodeAction(uri)
-		actions = append(actions, action)
+		actions = append(actions, *action)
 	}
+
+	actions = append(actions, *singleLineCodeAction(document, uri))
 
 	response := &lsp.CodeActionResponse{
 		Response: lsp.Response{
@@ -28,8 +33,8 @@ func handleCodeAction(request *lsp.CodeActionRequest, state *State) *lsp.CodeAct
 	return response
 }
 
-func shebangCodeAction(uri string) lsp.CodeAction {
-	action := lsp.CodeAction{
+func shebangCodeAction(uri string) *lsp.CodeAction {
+	action := &lsp.CodeAction{
 		Title: "Add shebang",
 		Edit: lsp.WorkspaceEdit{
 			Changes: map[string][]lsp.TextEdit{
@@ -46,6 +51,44 @@ func shebangCodeAction(uri string) lsp.CodeAction {
 							},
 						},
 						NewText: SHEBANG,
+					},
+				},
+			},
+		},
+	}
+	return action
+}
+
+func singleLineCodeAction(document string, uri string) *lsp.CodeAction {
+	fileAst, err := parseDocument(document, uri)
+	if err != nil {
+		return nil
+	}
+
+	singleLine := syntax.SingleLine(true)
+
+	printer := syntax.NewPrinter(singleLine)
+
+	buffer := bytes.NewBuffer([]byte{})
+	printer.Print(buffer, fileAst)
+
+	action := &lsp.CodeAction{
+		Title: "Single Line",
+		Edit: lsp.WorkspaceEdit{
+			Changes: map[string][]lsp.TextEdit{
+				uri: {
+					lsp.TextEdit{
+						Range: lsp.Range{
+							Start: lsp.Position{
+								Line:      0,
+								Character: 0,
+							},
+							End: lsp.Position{
+								Line:      9999,
+								Character: 9999,
+							},
+						},
+						NewText: buffer.String(),
 					},
 				},
 			},
