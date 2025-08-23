4912fbf06114d13d0df6d1dc8f2741aa89f800fe
matkrin <matkrin@protonmail.com>
2025-08-23 12:20:25 +0200

    feat(watch): Linux

--- watch.sh
+++ watch.sh
@@ -1,4 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+WATCH_DIR="."
+BUILD_CMD="go build"
 
 black-on-green() {
     printf "\e[30;42m%s\e[0m" "$1"
@@ -8,15 +11,42 @@ yellow-bold() {
     printf "\e[93m%s\e[0m" "$1"
 }
 
-watch-go-files() {
-    msg=$(yellow-bold "Watching .go files")
-    echo "$msg"
-    fswatch --exclude ".*" --include "\\.go$" . |
-        while read -r; do
+WATCH_MSG=$(yellow-bold "Watching .go files")
+BUILD_MSG=$(black-on-green "✓ Build successful")
+
+watch-go-files-mac() {
+    echo "$WATCH_MSG"
+    fswatch --exclude ".*" --include "\\.go$" $WATCH_DIR |
+        while read -r changed_file; do
             clear
-            echo "$msg"
-            go build . && black-on-green "✓ Build successful"
+            echo "$WATCH_MSG"
+            echo "Change detected in $changed_file. Rebuilding..."
+            go build . && echo "$BUILD_MSG"
         done
 }
 
-watch-go-files
+watch-go-files-linux() {
+    while true; do
+        echo "$WATCH_MSG"
+        changed_file=$(inotifywait \
+            -e modify,create,delete,move \
+            -r --exclude '(^|/)\.git/' \
+            --format '%w%f' \
+            "$WATCH_DIR" \
+        )
+
+        if [[ "$changed_file" == *.go ]]; then
+            clear
+            echo "Change detected in $changed_file. Rebuilding..."
+            $BUILD_CMD . && echo "$BUILD_MSG"
+        fi
+    done
+}
+
+if [[ $(uname) == "Linux" ]]; then
+    command -v inotifywait &>/dev/null || (echo "'inotifywait' needs to be installed" && exit)
+    watch-go-files-linux
+elif [[ $(uname) == "Darwin" ]]; then
+    command -v fswatch &>/dev/null || (echo "'fswatch' needs to be installed" && exit)
+    watch-go-files-mac
+fi
