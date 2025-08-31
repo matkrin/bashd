40db36af59141021b270dbec3af4fb091f09d5ea
matkrin <matkrin@protonmail.com>
2025-08-31 10:16:47 +0200

    refactor(workspace symbol): Cleanup

--- server/workspace_symbol.go
+++ server/workspace_symbol.go
@@ -8,7 +8,6 @@ import (
 	"mvdan.cc/sh/v3/syntax"
 )
 
-// TODO: Only for .sh files (could also do files without extension and check #!)
 func handleWorkspaceSymbol(request *lsp.WorkspaceSymbolRequest, state *State) *lsp.WorkspaceSymbolResponse {
 	slog.Info("Workspace Symbols Query", "query", request.Params.Query)
 	shFiles := state.WorkspaceShFiles()
@@ -22,50 +21,49 @@ func handleWorkspaceSymbol(request *lsp.WorkspaceSymbolRequest, state *State) *l
 		}
 		fileAst, err := parseDocument(string(fileContent), shFile)
 
-		for _, node := range defNodes(fileAst) {
-			var kind lsp.SymbolKind
-			var startLine, startCol, endLine, endCol uint
-			switch n := node.Node.(type) {
-			case *syntax.FuncDecl:
-				kind = lsp.SymbolFunction
-
-				startLine = n.Pos().Line() - 1
-				startCol = n.Pos().Col() - 1
-				endLine = n.Body.End().Line() - 1
-				endCol = n.Body.End().Col() - 1
-
-			case *syntax.Assign:
-				kind = lsp.SymbolVariable
-
-				startLine = n.Pos().Line() - 1
-				startCol = n.Pos().Col() - 1
-				endLine = n.End().Line() - 1
-				endCol = n.End().Col() - 1
-
-			}
-
-			workspaceSymbols = append(workspaceSymbols, lsp.WorkspaceSymbol{
-				Name: node.Name,
-				Kind: kind,
-				Location: lsp.Location{
-					URI: pathToURI(shFile),
-					Range: lsp.NewRange(
-						startLine,
-						startCol,
-						endLine,
-						endCol,
-					),
-				},
-			})
+		for _, defNode := range defNodes(fileAst) {
+			workspaceSymbols = append(
+				workspaceSymbols,
+				findWorkSpaceSymbol(&defNode, shFile),
+			)
 		}
 	}
-	response := lsp.WorkspaceSymbolResponse{
-		Response: lsp.Response{
-			RPC: "2.0",
-			ID:  &request.ID,
-		},
-		Result: workspaceSymbols,
-	}
+	response := lsp.NewWorkspaceSymbolResponse(request.ID, workspaceSymbols)
 	return &response
+}
 
+func findWorkSpaceSymbol(defNode *DefNode, filePath string) lsp.WorkspaceSymbol {
+	var kind lsp.SymbolKind
+	var  endLine, endCol uint
+	switch n := defNode.Node.(type) {
+	case *syntax.FuncDecl:
+		kind = lsp.SymbolFunction
+
+		endLine = n.Body.End().Line() - 1
+		endCol = n.Body.End().Col() - 1
+
+	case *syntax.Assign:
+		kind = lsp.SymbolVariable
+
+		endLine = n.End().Line() - 1
+		endCol = n.End().Col() - 1
+
+	}
+
+	startLine := defNode.Node.Pos().Line() -1
+	startCol := defNode.Node.Pos().Col() - 1
+
+	return lsp.WorkspaceSymbol{
+		Name: defNode.Name,
+		Kind: kind,
+		Location: lsp.Location{
+			URI: pathToURI(filePath),
+			Range: lsp.NewRange(
+				startLine,
+				startCol,
+				endLine,
+				endCol,
+			),
+		},
+	}
 }
