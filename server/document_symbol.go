b02e9cf9c9b3806b12182e8a992b61f2c3508bf5
matkrin <matkrin@protonmail.com>
2025-08-31 10:16:59 +0200

    refactor(document symbol): Cleanup

--- server/document_symbol.go
+++ server/document_symbol.go
@@ -5,69 +5,61 @@ import (
 	"mvdan.cc/sh/v3/syntax"
 )
 
-func handleDocumentSymbol(request *lsp.DocumentSymbolsRequest, state *State) lsp.DocumentSymbolResponse {
+func handleDocumentSymbol(request *lsp.DocumentSymbolsRequest, state *State) *lsp.DocumentSymbolResponse {
 	uri := request.Params.TextDocument.URI
 	document := state.Documents[uri]
 	documentSymbols := []lsp.DocumentSymbol{}
 	fileAst, err := parseDocument(document.Text, uri)
 	if err != nil {
-		return lsp.DocumentSymbolResponse{
-			Response: lsp.Response{
-				RPC: "2.0",
-				ID:  &request.ID,
-			},
-			Result: documentSymbols,
-		}
+		return nil
 	}
 
-	for _, node := range defNodes(fileAst) {
-		var kind lsp.SymbolKind
-		var startLine, startCol, endLine, endCol uint
-		var selectionStartLine, selectionStartCol, selectionEndLine, selectionEndCol uint
-		switch n := node.Node.(type) {
-		case *syntax.FuncDecl:
-			kind = lsp.SymbolFunction
-
-			startLine = n.Pos().Line() - 1
-			startCol = n.Pos().Col() - 1
-			endLine = n.Body.End().Line() - 1
-			endCol = n.Body.End().Col() - 1
-
-			selectionStartLine = n.Pos().Line() - 1
-			selectionStartCol = n.Pos().Col() - 1
-			selectionEndLine = n.End().Line() - 1
-			selectionEndCol = n.End().Col() - 1
-
-		case *syntax.Assign:
-			kind = lsp.SymbolVariable
-
-			startLine = n.Pos().Line() - 1
-			startCol = n.Pos().Col() - 1
-			endLine = n.End().Line() - 1
-			endCol = n.End().Col() - 1
-
-			selectionStartLine = n.Name.Pos().Line() - 1
-			selectionStartCol = n.Name.Pos().Col() - 1
-			selectionEndLine = n.Name.End().Line() - 1
-			selectionEndCol = n.Name.End().Col() - 1
-		}
-
-		documentSymbols = append(documentSymbols, lsp.DocumentSymbol{
-			Name:  node.Name,
-			Kind:  kind,
-			Range: lsp.NewRange(startLine, startCol, endLine, endCol),
-			SelectionRange: lsp.NewRange(
-				selectionStartLine, selectionStartCol, selectionEndLine, selectionEndCol,
-			),
-			Children: []lsp.DocumentSymbol{},
-		})
+	for _, defNode := range defNodes(fileAst) {
+		documentSymbols = append(documentSymbols, findDocumentSymbol(&defNode))
 	}
 
-	return lsp.DocumentSymbolResponse{
-		Response: lsp.Response{
-			RPC: "2.0",
-			ID:  &request.ID,
-		},
-		Result: documentSymbols,
+	response := lsp.NewDocumentSymbolResponse(request.ID, documentSymbols)
+	return &response
+}
+
+func findDocumentSymbol(defNode *DefNode) lsp.DocumentSymbol {
+	var kind lsp.SymbolKind
+	var endLine, endCol uint
+	var selectionStartLine, selectionStartCol, selectionEndLine, selectionEndCol uint
+	switch n := defNode.Node.(type) {
+	case *syntax.FuncDecl:
+		kind = lsp.SymbolFunction
+
+		endLine = n.Body.End().Line() - 1
+		endCol = n.Body.End().Col() - 1
+
+		selectionStartLine = n.Pos().Line() - 1
+		selectionStartCol = n.Pos().Col() - 1
+		selectionEndLine = n.End().Line() - 1
+		selectionEndCol = n.End().Col() - 1
+
+	case *syntax.Assign:
+		kind = lsp.SymbolVariable
+
+		endLine = n.End().Line() - 1
+		endCol = n.End().Col() - 1
+
+		selectionStartLine = n.Name.Pos().Line() - 1
+		selectionStartCol = n.Name.Pos().Col() - 1
+		selectionEndLine = n.Name.End().Line() - 1
+		selectionEndCol = n.Name.End().Col() - 1
+	}
+
+	startLine := defNode.Node.Pos().Line() -1
+	startCol := defNode.Node.Pos().Col() - 1
+
+	return lsp.DocumentSymbol{
+		Name:  defNode.Name,
+		Kind:  kind,
+		Range: lsp.NewRange(startLine, startCol, endLine, endCol),
+		SelectionRange: lsp.NewRange(
+			selectionStartLine, selectionStartCol, selectionEndLine, selectionEndCol,
+		),
+		Children: []lsp.DocumentSymbol{},
 	}
 }
